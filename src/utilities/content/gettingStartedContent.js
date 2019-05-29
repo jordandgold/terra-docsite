5cb59d66f5ad3cfdea62a07b8b66cc3cae5f5e2b
Kevin Simpson <kevin_simpson@trimble.com>
2019-05-29 14:11:13 -0600

    Add getting-started

---
+++ src/utilities/content/gettingStartedContent.js
@@ -0,0 +1,13 @@
+export const main =
+  "Terra is Trimble's design system. It was developed to help create a cohesive web experience across Trimble's properties. At its core, Terra was built out to be easy to use, intuitive, and serve as a great backbone for design";
+
+export const sectionOne = {
+  header: "Who is Terra designed by",
+  text:
+    "Terra was designed by Trimble's Creative Strategy Team. Terra is a living document, and it is continually being refined, refactored, and updated. We welcome questions, comments, and concerns"
+};
+
+export const sectionTwo = {
+  header: "Version 1.0",
+  text: "Version 1.0 is the first version of Terra"
+};
